e63ad175bdea21fc062c0bd28409d692da6aeb28
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-07-20 11:27:38 +0530

    new test instance per method no shared variable outcome

--- src/test/java/com/mkr/MethodOrderRandomTests.java
+++ src/test/java/com/mkr/MethodOrderRandomTests.java
@@ -1,35 +1,44 @@
 package com.mkr;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
 //@TestMethodOrder(MethodOrderer.Random.class) // Random order for test execution
 //@TestMethodOrder(MethodOrderer.MethodName.class)
+@TestInstance(TestInstance.Lifecycle.PER_METHOD) // Default: new instance for each method
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MethodOrderRandomTests {
+
+    StringBuilder builder = new StringBuilder();
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("The state of instance after each test: " + builder.toString());
+    }
 
     @Order(1)
     @Test
     void testD() {
         System.out.println("Running test D");
+        builder.append("1");
     }
 
     @Test
     void testA() {
         System.out.println("Running test A");
+        builder.append("3");
     }
 
     @Test
     void testB() {
         System.out.println("Running test B");
+        builder.append("4");
     }
 
     @Order(2)
     @Test
     void testC() {
         System.out.println("Running test C");
+        builder.append("2");
     }
 
 }
