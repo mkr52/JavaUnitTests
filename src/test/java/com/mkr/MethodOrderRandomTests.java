4af476af1c0ea07f4e6653b8fef380dfd437b73a
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-07-20 11:04:32 +0530

    random order to run tests

---
+++ src/test/java/com/mkr/MethodOrderRandomTests.java
@@ -0,0 +1,29 @@
+package com.mkr;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.Random.class) // Random order for test execution
+public class MethodOrderRandomTests {
+
+    @Test
+    void testA() {
+        System.out.println("Running test A");
+    }
+
+    @Test
+    void testB() {
+        System.out.println("Running test B");
+    }
+
+    @Test
+    void testC() {
+        System.out.println("Running test C");
+    }
+
+    @Test
+    void testD() {
+        System.out.println("Running test D");
+    }
+}
